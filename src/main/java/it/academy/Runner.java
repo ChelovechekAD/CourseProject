e68400c2b1f404c7aa119f66ee02d21249b52ae1
Chelovek <yushkevich2004@mail.ru>
2024-04-11 17:40:29 +0300

    last + checkstyle

--- src/main/java/it/academy/Runner.java
+++ src/main/java/it/academy/Runner.java
@@ -1,13 +1,18 @@
 package it.academy;
 
 import it.academy.DAO.CategoryDAO;
+import it.academy.DAO.ProductDAO;
 import it.academy.DAO.RoleDAO;
 import it.academy.DAO.impl.CategoryDAOImpl;
+import it.academy.DAO.impl.ProductDAOImpl;
 import it.academy.DAO.impl.RoleDAOImpl;
 import it.academy.enums.RoleEnum;
 import it.academy.models.Category;
+import it.academy.models.Product;
 import it.academy.models.Role;
 import it.academy.utilities.TransactionHelper;
+
+import java.util.stream.IntStream;
 
 public class Runner {
 
@@ -30,16 +35,16 @@ public class Runner {
         //userDAO.delete(1L);
 
         CategoryDAO categoryDAO = new CategoryDAOImpl(transactionHelper);
-        transactionHelper.transaction(() -> categoryDAO.create(Category.builder().categoryName("Стулья").build()));
+/*        transactionHelper.transaction(() -> categoryDAO.create(Category.builder().categoryName("Стулья").build()));
         transactionHelper.transaction(() -> categoryDAO.create(Category.builder().categoryName("Столы").build()));
         transactionHelper.transaction(() -> categoryDAO.create(Category.builder().categoryName("Шкафы").build()));
         transactionHelper.transaction(() -> categoryDAO.create(Category.builder().categoryName("Диваны").build()));
-        transactionHelper.transaction(() -> categoryDAO.create(Category.builder().categoryName("Двери").build()));
+        transactionHelper.transaction(() -> categoryDAO.create(Category.builder().categoryName("Двери").build()));*/
 
         
         
-        /*ProductDAO productDAO = new ProductDAOImpl(transactionHelper);
-
+        ProductDAO productDAO = new ProductDAOImpl(transactionHelper);
+        Category category = categoryDAO.get(2L);
        
 
         
@@ -47,23 +52,21 @@ public class Runner {
         IntStream.range(0, 120)
                 .forEach(i->{
                     Runnable runnable = () -> {
-                        Product product = productDAO.get(1L);
                         Product product1 = Product.builder()
-                                .price(product.getPrice())
-                                .name("Стол белый " + i)
-                                .categoryId(product.getCategoryId())
-                                .description(product.getDescription())
-                                .price(product.getPrice() + 100)
-                                .imageLink(product.getImageLink())
-                                .rating(product.getRating())
+                                .price(19329.9+i*10)
+                                .name("Стул белый " + i)
+                                .categoryId(category)
+                                .description("Loreal parisssssh aga net no da disc est okey da nado bolshe")
+                                .imageLink("chair-grey.jpg")
+                                .rating(10D)
                                 .build();
                         productDAO.create(product1);
                     };
                     transactionHelper.transaction(runnable);
                 });
-*/
 
 
+/*
         RoleDAO roleDAO = new RoleDAOImpl(transactionHelper);
         Role role = Role.builder()
                 .role(RoleEnum.ADMIN)
@@ -72,7 +75,7 @@ public class Runner {
             roleDAO.create(role);
         };
 
-        transactionHelper.transaction(runnable);
+        transactionHelper.transaction(runnable);*/
     }
 
 }
